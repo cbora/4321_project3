1c65dab4191483eacdb43a9c42eac12e3bf728b5
Richard Henwood <rbh288@cornell.edu>
2016-09-21 10:29:16 -0400

    Started a tuple class

---
+++ src/Project/Pair.java
@@ -0,0 +1,13 @@
+package Project;
+
+public class Pair {
+	
+	public String colName;
+	public int value;
+	
+	public Pair(String colName, int value) {
+		this.colName = colName;
+		this.value = value;
+	}
+
+}
--- src/Project/Tuple.java
+++ src/Project/Tuple.java
@@ -1,5 +1,58 @@
 package Project;
 
 public class Tuple {
-
+	/* --------------------------------------------------------------------------------- *
+	 * May not need to store colName AND value											 *
+	 * We may just need value...not sure yet, so included both. Adjust later if needed.  *
+	 * --------------------------------------------------------------------------------- */
+	
+	private Pair[] values; // represents tuple as array of (colName, value) pairs
+	
+	public Tuple(int nCols) {
+		values = new Pair[nCols];
+	}
+	
+	/**
+	 * Retrieves (colName, value) pair at index in tuple
+	 * @param index - where we are retrieving from
+	 * @return pair at index
+	 */
+	public Pair getPair(int index) {
+		return values[index];
+	}
+	
+	/**
+	 * Retrieves value of pair at index in tuple
+	 * @param index - where we are retrieving from
+	 * @return value of pair at index
+	 */
+	public int getVal(int index) {
+		return values[index].value;
+	}
+	
+	/**
+	 * Retrieves colName of pair at index in tuple
+	 * @param index - where we are retrieving from
+	 * @return colName of pair at index
+	 */
+	public String getColName(int index) {
+		return values[index].colName;
+	}
+	
+	/**
+	 * Inserts pair at index in tuple
+	 * @param p - pair we are inserting
+	 * @param index - where we want to insert
+	 */
+	public void add(Pair p, int index) {
+		values[index] = p;
+	}
+	
+	/**
+	 * retrieves length of tuple
+	 * @return length of tuple
+	 */
+	public int length() {
+		return values.length;
+	}
 }
