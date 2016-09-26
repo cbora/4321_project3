cfa5257ca63b77f4993d43b08c821d8688a47077
Richard Henwood <rbh288@cornell.edu>
2016-09-26 16:11:25 -0400

    SortedDupElim

--- src/Operators/SortedDupElimOperator.java
+++ src/Operators/SortedDupElimOperator.java
@@ -8,13 +8,15 @@ public class SortedDupElimOperator extends Operator {
 	
 	private Operator child;
 	private HashMap<String, Integer> schema;
-
+	private Tuple lastUnique;
+	
 	/* ================================== 
 	 * Constructors
 	 * ================================== */
 	public SortedDupElimOperator(Operator child) {
 		this.child = child;
 		this.schema = this.child.getSchema();
+		this.lastUnique = this.child.getNextTuple();
 	}
 	
 	/* ================================== 
@@ -27,13 +29,17 @@ public class SortedDupElimOperator extends Operator {
 
 	@Override
 	public Tuple getNextTuple() {
-		// TODO Auto-generated method stub
-		return null;
+		Tuple temp = this.lastUnique;
+		while (this.lastUnique != null && this.lastUnique.equals(temp)) {
+			this.lastUnique = this.child.getNextTuple();
+		}
+		return temp;
 	}
 
 	@Override
 	public void reset() {
 		this.child.reset();
+		this.lastUnique = this.child.getNextTuple();
 	}
 
 	@Override
--- src/Testing/SortedDupElimOperatorTest.java
+++ src/Testing/SortedDupElimOperatorTest.java
@@ -5,8 +5,8 @@ import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
-import Operators.HashDupElimOperator;
 import Operators.ScanOperator;
+import Operators.SortOperator;
 import Operators.SortedDupElimOperator;
 import Project.TableInfo;
 import Project.Tuple;
@@ -15,7 +15,7 @@ public class SortedDupElimOperatorTest {
 
 	@Test
 	public void duplicates() {
-		TableInfo tableInfo = new TableInfo("src/Duplicates","Duplicated");
+		TableInfo tableInfo = new TableInfo("src/SortedDuplicates","SortedDuplicated");
 		tableInfo.getColumns().add("X");
 		tableInfo.getColumns().add("Y");
 		tableInfo.getColumns().add("Z");
@@ -27,11 +27,11 @@ public class SortedDupElimOperatorTest {
 		t = de.getNextTuple();
 		assertEquals(t.toString(), "100,2,3");
 		t = de.getNextTuple();
+		assertEquals(t.toString(), "100,6,2");
+		t = de.getNextTuple();
 		assertEquals(t.toString(), "101,5,4");
 		t = de.getNextTuple();
 		assertEquals(t.toString(), "105,1,9");
-		t = de.getNextTuple();
-		assertEquals(t.toString(), "100,6,2");
 		t = de.getNextTuple();
 		assertNull(t);
 		
@@ -40,7 +40,7 @@ public class SortedDupElimOperatorTest {
 	
 	@Test
 	public void noDuplicates() {
-		TableInfo tableInfo = new TableInfo("src/Boats","Boats");
+		TableInfo tableInfo = new TableInfo("src/SortedBoats","SortedBoats");
 		tableInfo.getColumns().add("D");
 		tableInfo.getColumns().add("E");
 		tableInfo.getColumns().add("F");
@@ -53,9 +53,9 @@ public class SortedDupElimOperatorTest {
 		t = de.getNextTuple();
 		assertEquals(t.toString(), "102,3,4");
 		t = de.getNextTuple();
-		assertEquals(t.toString(), "104,104,2");
-		t = de.getNextTuple();
 		assertEquals(t.toString(), "103,1,1");
+		t = de.getNextTuple();
+		assertEquals(t.toString(), "104,104,2");
 		t = de.getNextTuple();
 		assertEquals(t.toString(), "107,2,8");
 		t = de.getNextTuple();
@@ -65,7 +65,7 @@ public class SortedDupElimOperatorTest {
 	
 	@Test
 	public void reset1() {
-		TableInfo tableInfo = new TableInfo("src/Duplicates","Duplicated");
+		TableInfo tableInfo = new TableInfo("src/SortedDuplicates","SortedDuplicates");
 		tableInfo.getColumns().add("X");
 		tableInfo.getColumns().add("Y");
 		tableInfo.getColumns().add("Z");
@@ -77,19 +77,19 @@ public class SortedDupElimOperatorTest {
 		t = de.getNextTuple();
 		assertEquals(t.toString(), "100,2,3");
 		t = de.getNextTuple();
-		assertEquals(t.toString(), "101,5,4");
+		assertEquals(t.toString(), "100,6,2");
 		de.reset();
 		t = de.getNextTuple();
 		assertEquals(t.toString(), "100,2,3");
 		t = de.getNextTuple();
-		assertEquals(t.toString(), "101,5,4");
+		assertEquals(t.toString(), "100,6,2");
 		
 		de.close();
 	}
 	
 	@Test
 	public void duplicates2() {
-		TableInfo tableInfo = new TableInfo("src/Duplicates","Duplicated");
+		TableInfo tableInfo = new TableInfo("src/SortedDuplicates","SortedDuplicated");
 		tableInfo.getColumns().add("X");
 		tableInfo.getColumns().add("Y");
 		tableInfo.getColumns().add("Z");
@@ -102,11 +102,11 @@ public class SortedDupElimOperatorTest {
 			t = de.getNextTuple();
 			assertEquals(t.toString(), "100,2,3");
 			t = de.getNextTuple();
+			assertEquals(t.toString(), "100,6,2");
+			t = de.getNextTuple();
 			assertEquals(t.toString(), "101,5,4");
 			t = de.getNextTuple();
 			assertEquals(t.toString(), "105,1,9");
-			t = de.getNextTuple();
-			assertEquals(t.toString(), "100,6,2");
 			t = de.getNextTuple();
 			assertNull(t);
 			de.reset();
@@ -117,22 +117,25 @@ public class SortedDupElimOperatorTest {
 	
 	@Test
 	public void testGetSchema() {
-		TableInfo tableInfo = new TableInfo("src/Reserves","Reserves");
-		tableInfo.getColumns().add("G");
-		tableInfo.getColumns().add("H");
+		TableInfo tableInfo = new TableInfo("src/SortedDuplicates","SortedDuplicates");
+		tableInfo.getColumns().add("X");
+		tableInfo.getColumns().add("Y");
+		tableInfo.getColumns().add("Z");
 		
 		ScanOperator scn1 = new ScanOperator(tableInfo);
 		SortedDupElimOperator de1 = new SortedDupElimOperator(scn1);
-		assertEquals((int) de1.getSchema().get("Reserves.G"), 0);
-		assertEquals((int) de1.getSchema().get("Reserves.H"), 1);
-		assertNull(de1.getSchema().get("Reserves.Z"));
+		assertEquals((int) de1.getSchema().get("SortedDuplicates.X"), 0);
+		assertEquals((int) de1.getSchema().get("SortedDuplicates.Y"), 1);
+		assertEquals((int) de1.getSchema().get("SortedDuplicates.Z"), 2);
+		assertNull(de1.getSchema().get("SortedDuplicates.A"));
 		
-		ScanOperator scn2 = new ScanOperator(tableInfo, "R");
+		ScanOperator scn2 = new ScanOperator(tableInfo, "SD");
 		SortedDupElimOperator de2 = new SortedDupElimOperator(scn2);
-		assertEquals((int) de2.getSchema().get("R.G"), 0);
-		assertEquals((int) de2.getSchema().get("R.H"), 1);
-		assertNull(de2.getSchema().get("Reserves.G"));
-		assertNull(de2.getSchema().get("R.Z"));
+		assertEquals((int) de2.getSchema().get("SD.X"), 0);
+		assertEquals((int) de2.getSchema().get("SD.Y"), 1);
+		assertEquals((int) de2.getSchema().get("SD.Z"), 2);
+		assertNull(de2.getSchema().get("SortedDuplicates.X"));
+		assertNull(de2.getSchema().get("SortedDuplicates.A"));
 	}
 
 }
