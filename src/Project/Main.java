e1529f738c73530e55cd5ef7b26d97338395debb
Richard Henwood <rbh288@cornell.edu>
2016-10-03 18:13:21 -0400

    Takes cmd line args

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -1,53 +1,39 @@
 package Project;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.PrintWriter;
-import java.io.FileReader;
+
+import Operators.Operator;
 import net.sf.jsqlparser.parser.CCJSqlParser;
 import net.sf.jsqlparser.statement.Statement;
-import net.sf.jsqlparser.statement.select.Select;
-
-
-import static org.junit.Assert.*;
-
-import java.io.StringReader;
-import java.util.ArrayList;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import Operators.HashDupElimOperator;
-import Operators.Operator;
-import Operators.ScanOperator;
-import Operators.SortOperator;
-import Project.DbCatalog;
-import Project.Driver;
-import Project.TableInfo;
-import Project.Tuple;
-import net.sf.jsqlparser.parser.CCJSqlParser;
-import net.sf.jsqlparser.statement.select.OrderByElement;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
 
+/**
+ * Entry point to application
+ * @author Richard Henwood (rbh228)
+ * @author Chris Bora (cdb239)
+ * @author Han Wen Chen (hc844)
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		DbCatalog dbC = DbCatalog.getInstance(); 
+		final String inputDir = args[0];
+		final String outputDir = args[1];
 		final String queriesFile = "queries.sql";
-
 		
 		try {
-			//BufferedReader br = new BufferedReader(new FileReader(args[0]+"db/schema.txt"));
-			BufferedReader br = new BufferedReader(new FileReader("src/db/schema.txt"));
+			BufferedReader br = new BufferedReader(new FileReader(inputDir + "/db/schema.txt"));
 
-			
 			String read = br.readLine();
 		   
 		    while (read  != null) {
 		    	String[] line  = read.split(" "); 
-		    	//TableInfo t = new TableInfo(args[0]+"db/data/" + line[0], line[0]);
-		    	TableInfo t = new TableInfo("src/db/data/" + line[0], line[0]);
+		    	TableInfo t = new TableInfo(inputDir + "/db/data/" + line[0], line[0]);
 		    	for (int i=1; i < line.length; i++){
 		    		t.getColumns().add(line[i]); 
 		    	}
@@ -63,8 +49,7 @@ public class Main {
 		 }
 		
 		try {
-			//CCJSqlParser parser = new CCJSqlParser(new FileReader(args[0]+"/"+queriesFile));
-			CCJSqlParser parser = new CCJSqlParser(new FileReader("src/"+queriesFile));
+			CCJSqlParser parser = new CCJSqlParser(new FileReader(inputDir + "/" + queriesFile));
 
 			Statement statement;
 			int queryNum = 1;
@@ -78,9 +63,9 @@ public class Main {
 					Driver d = new Driver(body);
 					Operator o = d.getRoot();
 					Tuple t = o.getNextTuple();
-					//PrintWriter writer = new PrintWriter(args[1]+"/query"+ queryNum, "UTF-8");
 	
-					PrintWriter writer = new PrintWriter("src/query"+ queryNum, "UTF-8");
+					File fileOut = new File(outputDir + "/query" + queryNum);
+					PrintWriter writer = new PrintWriter(fileOut);
 	
 					while (t != null){
 						writer.println(t.toString());
