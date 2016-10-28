6f98fc66ae5dbad879a1915e87ba94a334923235
Richard Henwood <rbh288@cornell.edu>
2016-10-28 12:41:36 -0400

    BNLJ done :)

--- src/Operators/BNLJoinOperator.java
+++ src/Operators/BNLJoinOperator.java
@@ -1,7 +1,5 @@
 package Operators;
 
-import java.util.HashMap;
-
 import Project.Tuple;
 import net.sf.jsqlparser.expression.Expression;
 
@@ -13,6 +11,7 @@ public class BNLJoinOperator extends JoinOperator {
 	private static int PAGE_SIZE = 4096; // bytes per page
 	private static int INT_SIZE = Integer.SIZE / Byte.SIZE; // bytes per int
 	private Tuple[] buffer; // tuple buffer
+	private int bIndex; // position in tuple buffer
 	
 	/* ================================== 
 	 * Constructors
@@ -26,7 +25,9 @@ public class BNLJoinOperator extends JoinOperator {
 	 */
 	public BNLJoinOperator(Operator leftChild, Operator rightChild, Expression exp, int bSize) {
 		super(leftChild, rightChild, exp);
-		buffer = new Tuple[(bSize * PAGE_SIZE) / (INT_SIZE * schema.size())];
+		this.buffer = new Tuple[(bSize * PAGE_SIZE) / (INT_SIZE * schema.size())];
+		this.bIndex = 0;
+		fillBuffer();
 	}
 	
 	/**
@@ -44,14 +45,46 @@ public class BNLJoinOperator extends JoinOperator {
 	 * ================================== */
 	@Override
 	public Tuple getNextTuple() {
-		// TODO Auto-generated method stub
-		return null;
+		Tuple right = rightChild.getNextTuple();
+		
+		// if reached end of right relation, reset right and increment bIndex
+		if (right == null) {
+			rightChild.reset();
+			right = rightChild.getNextTuple();
+			
+			// if right is still null, then right relation is empty, so return null
+			if (right == null)
+				return null;
+			
+			bIndex++;
+		}
+		
+		// if reached end of buffer, refill buffer
+		if (bIndex >= buffer.length) {
+			bIndex = 0;
+			fillBuffer();
+		}
+		
+		Tuple left = buffer[bIndex];
+
+		// if current spot in the buffer is null, we have looked at all tuples in left relation. Return null.
+		if (left == null) {
+			return null;
+		}
+		
+		// concatenate tuple from left relation with tuple from right relation
+		Tuple result = Tuple.concat(left, right);
+		
+		// return result if it passes join condition. otherwise call getNextTuple() again
+		return passesCondition(result) ? result : getNextTuple();
 	}
 
 	@Override
 	public void reset() {
-		// TODO Auto-generated method stub
-
+		leftChild.reset();
+		rightChild.reset();
+		bIndex = 0;
+		fillBuffer();
 	}
 
 	/**
@@ -61,9 +94,10 @@ public class BNLJoinOperator extends JoinOperator {
 		Tuple t;
 		int i = 0;
 		
-		while ((t = leftChild.getNextTuple()) != null && i < buffer.length) {
+		for (; i < buffer.length; i++) {
+			if ((t = leftChild.getNextTuple()) == null)
+				break;
 			buffer[i] = t;
-			i++;
 		}
 		for (; i < buffer.length; i++) {
 			buffer[i] = null;
